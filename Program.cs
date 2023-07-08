b8726ca595eb90ed4e250c5504f64381974d9877
Trent Stenoien <trent.stenoien@gmail.com>
2023-07-07 20:09:11 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ti4_calc
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+		}
+	}
+}
